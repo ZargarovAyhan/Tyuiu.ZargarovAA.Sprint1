ea06975dcd29bedeb1e54b9034e2fff773106662
Tyuiu.ZargarovAA@outlook.com <fairmarko@gmail.com>
2023-09-19 20:21:47 +0500

    Task0

---
+++ Tyuiu.ZargarovAA.Sprint1.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZargarovAA.Sprint1.V13.Lib;
+
+namespace Tyuiu.ZargarovAA.Sprint1.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(1, res);
+
+        }
+    }
+}
