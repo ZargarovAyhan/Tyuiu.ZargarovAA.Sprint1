dd06a279a8755832948432cdb01b1b415164b4a6
Tyuiu.ZargarovAA@outlook.com <fairmarko@gmail.com>
2023-09-19 15:17:11 +0500

    Add project files.

---
+++ Tyuiu.ZargarovAA.Sprint1.Task0.V13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZargarovAA.Sprint1.V13.Lib;
+
+namespace Tyuiu.ZargarovAA.Sprint1.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = Console.Title = "Спринт #0| Выполнил: Заргаров А. А. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы C#                                          *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил Заргаров А. А. | ИИПб-23-2                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на С#, которая вычисляет выражение 10/(2+3)          *");
+            Console.WriteLine("* и печатает результат на экране                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 10 / (2 + 3)                                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
