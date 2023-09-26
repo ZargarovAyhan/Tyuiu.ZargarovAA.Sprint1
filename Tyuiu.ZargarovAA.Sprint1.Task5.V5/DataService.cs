2898062662b1c9112e06e01118abde5a020274b2
Tyuiu.ZargarovAA@outlook.com <fairmarko@gmail.com>
2023-09-27 01:15:08 +0500

    Task5

---
+++ Tyuiu.ZargarovAA.Sprint1.Task5.V5/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ZargarovAA.Sprint1.Task5.V5.Lib
+{
+    public class DataService : ISprint1Task5V5
+    {
+        public int Calculate(double x)
+        {
+            int res = (int)((x % 1) * 10);
+            return res;
+        }
+    }
+}
